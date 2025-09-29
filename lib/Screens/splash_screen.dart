b8d28ec625b7d143ff743d31ec057d377e4ef468
Mustafa Mohamed Roshdy <120448988+Mustafa-Roshdy@users.noreply.github.com>
2025-09-29 16:30:20 +0300

    added animation to splash screen

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -8,37 +8,63 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _blinkAnimation;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
+    // Blink controller
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+
+    _blinkAnimation = Tween<double>(begin: 1.0, end: 0.2).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    // Run blink once (forward then reverse)
+    _controller.forward().then((_) => _controller.reverse());
+
+    // Navigate after 2 seconds
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacementNamed(context, "/login");
     });
   }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        // width: double.infinity,
         color: Colors.black,
         child: Center(
-          child: Column(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.remove_red_eye,size: 100,color: Colors.pink,),
-                  Gap(10),
-                  Icon(Icons.remove_red_eye,size: 100,color: Colors.pink,),
-                ],
-                
+              ScaleTransition(
+                scale: _blinkAnimation,
+                child: const Icon(
+                  Icons.remove_red_eye,
+                  size: 100,
+                  color: Colors.pink,
+                ),
               ),
-
+              const Gap(10),
+              const Icon(
+                Icons.remove_red_eye,
+                size: 100,
+                color: Colors.pink,
+              ),
             ],
           ),
         ),
