1fdff5b5e2e281c8d17893d9792639dc50e2743e
Mustafa Mohamed Roshdy <120448988+Mustafa-Roshdy@users.noreply.github.com>
2025-09-29 17:21:05 +0300

    added 3 pages to swap using BottomNavigationBar

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -10,6 +10,84 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      drawer: Drawer(
+        surfaceTintColor: Colors.pink,
+        shadowColor: Colors.pink,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.pink),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.pink,
+                    child: Icon(Icons.person, size: 50, color: Colors.white),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Mustafa Roshdy",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushReplacementNamed(context, "/login");
+              },
+            ),
+          ],
+        ),
+      ),
+
+      ///---------------------------AppBar------------------------------------
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: Icon(Icons.menu, color: Colors.pink),
+            );
+          },
+        ),
+        backgroundColor: Colors.black,
+        title: Text(
+          "Home",
+          style: TextStyle(
+            fontWeight: FontWeight.w800,
+            fontSize: 25,
+            color: Colors.pink,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/Screens/switch_screen.dart
+++ lib/Screens/switch_screen.dart
@@ -1,5 +1,8 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:product/Screens/add_screen.dart';
+import 'package:product/Screens/home_screen.dart';
+import 'package:product/Screens/profile_screen.dart';
 
 class SwitchScreen extends StatefulWidget {
   const SwitchScreen({super.key});
@@ -10,103 +13,26 @@ class SwitchScreen extends StatefulWidget {
 
 class _SwitchScreenState extends State<SwitchScreen> {
   int _page = 0;
-  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+  final List<Widget> _list = [HomeScreen(), AddScreen(), ProfileScreen()];
+  final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        surfaceTintColor: Colors.pink,
-        shadowColor: Colors.pink,
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.pink),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundColor: Colors.pink,
-                    child: Icon(Icons.person, size: 50, color: Colors.white),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    "Mustafa Roshdy",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text('Logout'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushReplacementNamed(context, "/login");
-              },
-            ),
-          ],
-        ),
-      ),
+      
 
-      ///---------------------------AppBar------------------------------------
-      appBar: AppBar(
-        leading: Builder(
-          builder: (context) {
-            return IconButton(
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-              icon: Icon(Icons.menu, color: Colors.pink),
-            );
-          },
-        ),
-        backgroundColor: Colors.black,
-        title: Text(
-          "Product",
-          style: TextStyle(
-            fontWeight: FontWeight.w800,
-            fontSize: 25,
-            color: Colors.pink,
-          ),
-        ),
-      ),
-
-      ///---------------------------bottomNavigationBar------------------------------------
+      ///---------------------------BottomNavigationBar------------------------------------
       bottomNavigationBar: CurvedNavigationBar(
         key: _bottomNavigationKey,
         backgroundColor: Colors.transparent,
         index: 0,
-        items: <Widget>[
+        items: const <Widget>[
           Icon(Icons.home_outlined, size: 30),
           Icon(Icons.add, size: 30),
           Icon(Icons.perm_identity, size: 30),
-          // Icon(Icons.compare_arrows, size: 30),
-          // Icon(Icons.call_split, size: 30),
         ],
         color: Colors.white,
         buttonBackgroundColor: Colors.white,
-        // backgroundColor: Colors.blueAccent,
         animationCurve: Curves.easeInOut,
         animationDuration: Duration(milliseconds: 600),
         onTap: (index) {
@@ -118,25 +44,7 @@ class _SwitchScreenState extends State<SwitchScreen> {
       ),
 
       ///---------------------------Body------------------------------------
-      body: Container(
-        // color: Colors.blueAccent,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(_page.toString(), style: TextStyle(fontSize: 160)),
-              ElevatedButton(
-                child: Text('Go To Page of index 1'),
-                onPressed: () {
-                  final CurvedNavigationBarState? navBarState =
-                      _bottomNavigationKey.currentState;
-                  navBarState?.setPage(1);
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: _list[_page], 
     );
   }
 }
